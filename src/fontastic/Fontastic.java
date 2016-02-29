eb0fa8638f6a992e88d8cde68124c8a927859a7a
Bianca Denise del Puerto <biancadenise.delpuerto@bicol-u.edu.ph>
2016-02-29 22:05:07 +0800

    Initial commit. With MainMenu and Drawing Interface. Start button functionality.

---
+++ src/fontastic/Fontastic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fontastic;
+
+/**
+ *
+ * @author BiancaDenise
+ */
+public class Fontastic extends MainFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame UserInterface = new MainFrame();
+        UserInterface.setVisible(true);
+        //UserInterface.setExtendedState(MainFrame.MAXIMIZED_BOTH);
+    }
+    
+}
